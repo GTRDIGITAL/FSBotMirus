3486e1dafc994c7d4881604250aeb69baa9eb6b1
denisdenis11 <denis.david@ro.gt.com>
2023-09-13 17:18:53 +0300

    change3333

--- app.py
+++ app.py
@@ -1800,7 +1800,7 @@ def my_form_post2():
 			return word
 		nr=0
 		abc=[]
-		denis=datetime.datetime.now()
+		# denis=datetime.datetime.now()
 		for i in files:
 			nr=nr+1
 			reader = PyPDF2.PdfFileReader(i)
@@ -1808,8 +1808,8 @@ def my_form_post2():
 			a=str(*dictionary.values())
 			b=a.replace('\\n','').replace("b'","").replace("></declaratie300>'","></declaratie300>").replace('encoding="utf-8"', "").replace('''"/>''', "</declaratie300>").replace("</declaratie300>'", '"></declaratie300>').replace('\\r', '')
 			# print(b)
-			f=open("/home/fsbot/storage/vat"+str(nr)+str(denis.year)+str(denis.month)+str(denis.day)+str(denis.hour)+str(denis.minute)+str(denis.second)+".xml","w").write(b)
-			abc.append("/home/fsbot/storage/vat"+str(nr)+str(denis.year)+str(denis.month)+str(denis.day)+str(denis.hour)+str(denis.minute)+str(denis.second)+".xml")
+			f=open("/home/fsbot/storage/vat"+str(nr)+".xml","w").write(b)
+			abc.append("/home/fsbot/storage/vat"+str(nr)+".xml")
 			
 		fisiere=list(abc)
 		for v in range(0,len(fisiere)):
@@ -6554,8 +6554,8 @@ def my_form_post1():
 			a=str(*dictionary.values())
 			b=a.replace('\\n','').replace("b'","").replace("></declaratie300>'","></declaratie300>").replace('encoding="utf-8"', "").replace('''"/>''', "</declaratie300>").replace("</declaratie300>'", '"></declaratie300>').replace('\\r', '')
 			# print(b)
-			f=open("/home/fsbot/storage/vat"+str(nr)+str(denis.year)+str(denis.month)+str(denis.day)+str(denis.hour)+str(denis.minute)+str(denis.second)+".xml","w").write(b)
-			abc.append("/home/fsbot/storage/vat"+str(nr)+str(denis.year)+str(denis.month)+str(denis.day)+str(denis.hour)+str(denis.minute)+str(denis.second)+".xml")
+			f=open("/home/fsbot/storage/vat"+str(nr)+".xml","w").write(b)
+			abc.append("/home/fsbot/storage/vat"+str(nr)+".xml")
 			
 		fisiere=list(abc)
 		for v in range(0,len(fisiere)):
@@ -11307,8 +11307,8 @@ def my_form_post4():
 			a=str(*dictionary.values())
 			b=a.replace('\\n','').replace("b'","").replace("></declaratie300>'","></declaratie300>").replace('encoding="utf-8"', "").replace('''"/>''', "</declaratie300>").replace("</declaratie300>'", '"></declaratie300>').replace('\\r', '')
 			# print(b)
-			f=open("/home/fsbot/storage/vat"+str(nr)+str(denis.year)+str(denis.month)+str(denis.day)+str(denis.hour)+str(denis.minute)+str(denis.second)+".xml","w").write(b)
-			abc.append("/home/fsbot/storage/vat"+str(nr)+str(denis.year)+str(denis.month)+str(denis.day)+str(denis.hour)+str(denis.minute)+str(denis.second)+".xml")
+			f=open("/home/fsbot/storage/vat"+str(nr)+".xml","w").write(b)
+			abc.append("/home/fsbot/storage/vat"+str(nr)+".xml")
 			
 		fisiere=list(abc)
 		for v in range(0,len(fisiere)):
